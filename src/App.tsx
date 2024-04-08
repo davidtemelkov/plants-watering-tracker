b91124ed86435b6d8cb421a081a06acd927b3208
davidtemelkov <dntemelkov@gmail.com>
2024-04-08 12:34:24 +0300

    Break it

--- src/App.tsx
+++ src/App.tsx
@@ -26,10 +26,10 @@ function App() {
   const modalRef = useRef<HTMLDivElement>(null);
 
   const dynamoDBClient = new DynamoDBClient({
-    region: import.meta.env.VITE_AWS_REGION!,
+    region: import.meta.env.AWS_REGION!,
     credentials: {
-      accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID!,
-      secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY!,
+      accessKeyId: import.meta.env.AWS_ACCESS_KEY_ID!,
+      secretAccessKey: import.meta.env.AWS_SECRET_ACCESS_KEY!,
     },
   });
 
